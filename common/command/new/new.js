33fd79ca7682b1950fb74660d7f026487d9d6625
EternalHeart <hao131462@qq.com>
2024-04-09 19:25:01 +0800

    feat: new finish basic

--- common/command/new/new.js
+++ common/command/new/new.js
@@ -3,6 +3,7 @@ import { npmInstall } from '#common/utils/manager/npm.js';
 import { readConfig, writeConfig } from '#common/utils/file/writeConfig.js';
 import { resolve } from 'node:path';
 import { pnpmInstall } from '#common/utils/manager/pnpm.js';
+import { createFile, readFile } from '#common/utils/file/create.js';
 
 export const newProject = async (projectName) => {
     await executeInteraction(`npx create-nx-workspace@latest ${projectName} --packageManager=pnpm`);
@@ -36,7 +37,8 @@ export const newProject = async (projectName) => {
             '--disableInternal',
             '--disableProtected',
             '--disableLifeCycleHooks'
-        ]
+        ],
+        styles: ['ui/src/lib/styles/public.scss']
     };
     Object.assign(projectJson.targets?.['storybook'].options, compodocConfig);
     Object.assign(projectJson.targets?.['build-storybook']?.options, compodocConfig);
@@ -44,27 +46,42 @@ export const newProject = async (projectName) => {
     // 4. style 创建 styles -> [ variables,public.scss ]
     const variablesPath = resolve('ui/styles/variables/publish.scss');
     const publishScssPath = resolve('ui/styles/publish.scss');
-    await writeConfig(variablesPath);
-    await writeConfig(publishScssPath);
+    await writeConfig(variablesPath, `@mixin variables() {\n}`);
+    await writeConfig(publishScssPath, `@import "./variables/public";\n@include variables();`);
 
-    // 设置 ui/project.json -> targets.[storybook,build-storybook].options +
-    //         "styles": [
-    //           "ui/src/lib/styles/public.scss"
-    //         ]
-
-    // 5. 在 ui/.storybook/preview.ts +
-    // import { setCompodocJson } from '@storybook/addon-docs/angular';
-    // import docJson from '../documentation.json';
-    // setCompodocJson(docJson);
-    // 创建文件  ui/documentation.json
-
+    // 5. 在 ui/.storybook/preview.ts  创建文件  ui/documentation.json
+    const previewPath = 'ui/.storybook/preview.ts';
+    const documentationPath = 'ui/documentation.json';
+    await writeConfig(documentationPath, '');
+    await writeConfig(
+        previewPath,
+        `import { setCompodocJson } from '@storybook/addon-docs/angular';\nimport docJson from '../documentation.json';\nsetCompodocJson(docJson);`
+    );
     // 6. ui/tsconfig.json + compilerOptions -> "resolveJsonModule": true
-
+    const tsconfigPath = 'ui/tsconfig.json';
+    const tsconfig = await readConfig(tsconfigPath);
+    tsconfig.compilerOptions.resolveJsonModule = true;
+    await writeConfig(tsconfigPath, tsconfig);
     // 7. ui/.storybook/main.ts +
     // docs: {
     //     autodocs: true,
     //     defaultName: '说明',
     //   },
+    const mainPath = 'ui/.storybook/main.ts';
+    const mainContent = await readFile(mainPath);
+    await createFile(mainPath, mainContent);
 
     // 8. 在package.json 中添加指令
+    const packageJsonPath = 'package.json';
+    const packageJson = await readConfig(packageJsonPath);
+    Object.assign(packageJson.scripts, {
+        'fix:nx': 'rm -rf .nx && rm -rf .angular',
+        'clean': 'rm -rf dist && rm -rf node_modules',
+        'play': 'nx run play:serve',
+        'doc:serve': 'nx run ui:storybook',
+        'doc:build': 'nx run ui:build-storybook',
+        'ui:build': 'nx run ui:build',
+        'ui:unpublish': 'npm unpublish cses-ui --registry http://npm.runtongqiuben.com --force',
+        'ui:publish': 'pnpm build && cd dist/cses-ui && npm publish --registry https://npm.runtongqiuben.com'
+    });
 };
--- common/utils/file/create.js
+++ common/utils/file/create.js
@@ -1,5 +1,6 @@
 import { logger } from '#common/utils/logger.js';
-import { mkdirSync, writeFileSync } from 'node:fs';
+import { mkdirSync, readFileSync, writeFileSync } from 'node:fs';
+import { resolve } from 'node:path';
 
 /**
  * 创建路径 - 无视路径
@@ -15,7 +16,20 @@ export const createDir = (path) => {
  * @param content
  */
 export const createFile = (filePath, content) => {
-    const dir = filePath.substring(0, filePath.lastIndexOf('/'));
-    createDir(dir);
-    writeFileSync(filePath, content);
+    return new Promise((resolve) => {
+        const dir = filePath.substring(0, filePath.lastIndexOf('/'));
+        createDir(dir);
+        writeFileSync(filePath, content);
+        resolve();
+    });
+};
+/**
+ * 读取文件
+ * @param filePath
+ * @returns {Promise<unknown>}
+ */
+export const readFile = (filePath) => {
+    return new Promise((resolve) => {
+        resolve(readFileSync(filePath, 'utf-8'));
+    });
 };

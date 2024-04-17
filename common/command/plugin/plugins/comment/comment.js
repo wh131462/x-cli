d2dc7dcbe960e46b47eed6af6171e4d7178414b1
EternalHeart <hao131462@qq.com>
2024-04-17 16:23:52 +0800

    chore: plugin comment

--- common/command/plugin/plugins/comment/comment.js
+++ common/command/plugin/plugins/comment/comment.js
@@ -2,6 +2,7 @@ import { rootPath } from '#common/utils/file/path.js';
 import { copyFile, readdir, rm } from 'node:fs/promises';
 import { resolve } from 'node:path';
 import { existsSync } from 'node:fs';
+import { logger } from '#common/utils/x/logger.js';
 
 /**
  * comment
@@ -16,6 +17,9 @@ export const comment = {
         }
     },
     install: async () => {
+        logger.info(
+            `文件下载至: file://${resolve(process.cwd(), 'settings.zip')}\n请按照操作路径进行导入:\n文件->管理IDE设置->导入设置->选择settings.zip文件->确认导入`
+        );
         await copyFile(resolve(rootPath, 'resources', 'settings.zip'), resolve(process.cwd(), 'settings.zip'));
     },
     uninstall: async () => {

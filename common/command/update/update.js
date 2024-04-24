442d2f5a782904392d5b391e97bdb58bf6ddc706
EternalHeart <hao131462@qq.com>
2024-04-24 11:34:47 +0800

    chore: change name from x-cli to @eh/cli

--- common/command/update/update.js
+++ common/command/update/update.js
@@ -1,5 +1,7 @@
 import { npmInstall } from '#common/utils/manager/npm.js';
+import { getPackageJson } from '#common/utils/file/getPackageJson.js';
 
-export const update = () => {
-    return npmInstall('x-cli', false, true);
+export const update = async () => {
+    const { name } = await getPackageJson();
+    return npmInstall(name, false, true);
 };

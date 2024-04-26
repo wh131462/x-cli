01138e4d28af7dbce27ca43767a41950de9d1f42
EternalHeart <hao131462@qq.com>
2024-04-26 16:58:38 +0800

    chore: remove test code

--- common/command/update/update.js
+++ common/command/update/update.js
@@ -1,8 +1,5 @@
 import { getPackageJson } from '#common/utils/file/getPackageJson.js';
-import { logger } from '#common/utils/x/logger.js';
-import { inX } from '#common/utils/x/where.js';
 import { npmInstall } from '#common/utils/manager/npm.js';
-import { rootPath } from '#common/utils/file/path.js';
 
 /**
  * 更新脚本
@@ -10,7 +7,5 @@ import { rootPath } from '#common/utils/file/path.js';
  */
 export const update = async () => {
     const { name } = await getPackageJson();
-    logger.info(name, rootPath);
-    console.log((await inX()) ? '在' : '不在');
     await npmInstall(name, false, true);
 };
